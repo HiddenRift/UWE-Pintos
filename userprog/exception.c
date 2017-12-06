1601ea2da2e5e973913840143753b2fbc9cb88c9
Robert Fry <robert3.fry@live.uwe.ac.uk>
2017-12-06 12:28:09 +0000

    fixed unneccessary killing thread when testing memory

--- userprog/exception.c
+++ userprog/exception.c
@@ -164,5 +164,5 @@ page_fault (struct intr_frame *f)
           not_present ? "not present" : "rights violation",
           write ? "writing" : "reading",
           user ? "user" : "kernel");
-  kill (f);
+  //kill (f);
 }
